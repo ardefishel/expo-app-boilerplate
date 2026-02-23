fa653f2ffc467bacba1de47ceab94cd5eadc812d
ardefishel (rtvcl) <ardefishel@gmail.com>
2026-02-23 21:20:22 +0700

    feat: Create a `uniwind`-wrapped `SafeAreaView` component and apply it in the home screen.

--- src/app/(app)/(tabs)/home/index.tsx
+++ src/app/(app)/(tabs)/home/index.tsx
@@ -1,16 +1,10 @@
+import { SafeAreaView } from "@/components/ui/safe-area-view";
 import { router } from "expo-router";
 import { Button } from "heroui-native";
 import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-
 export default function Index() {
   return (
-    <SafeAreaView
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+    <SafeAreaView className="flex justify-center flex-1 items-center"
     >
       <Button onPress={() => { router.push("/(app)/(modal)/action-sheet") }}>
         <Text>Action Sheet</Text>
